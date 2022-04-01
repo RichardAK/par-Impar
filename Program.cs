d9b7ba465f0b597dce5fcfe684ae63272878d795
Kauã <kau.lira@yahoo.com>
2022-04-01 20:44:32 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int numerocolocado;
+
+Console.Write("Digite um número:");
+numerocolocado = Convert.ToInt32(Console.ReadLine());
+
+bool numeropar = (numerocolocado % 2) == 0;
+
+if (numeropar)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine($"{numerocolocado} é par");
+    
+}
+
+else
+{
+    Console.WriteLine($"{numerocolocado} é impar");
+    Console.Beep();
+}
+
+Console.ResetColor();
